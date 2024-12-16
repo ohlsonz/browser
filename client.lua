e79029d0bedf0b257b7f57c82e73613789e9dca0
Leo Ohlson <leo@ohlson.dev>
2024-12-16 17:37:29 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,22 +1,10 @@
-if IsDuplicityVersion() then
-	--- @param playerId string | integer
-	--- @param value string
-	local function setClipboard(playerId, value)
-		if not DoesPlayerExist(playerId --[[@as string]]) then return end
+--- @param value string
+local function setClipboard(value)
+	assert(type(value) == "string", ("Failed to copy to clipboard, expected string. Got %s"):format(type(value)))
 
-		TriggerClientEvent("setClipboard", playerId --[[@as number]], value)
-	end
-
-	exports("setClipboard", setClipboard)
-else
-	--- @param value string
-	local function setClipboard(value)
-		SendNUIMessage({
-			value = value
-		})
-	end
-
-	RegisterNetEvent("setClipboard", setClipboard)
-
-	exports("setClipboard", setClipboard)
+	SendNUIMessage({
+		value = value
+	})
 end
+
+exports("setClipboard", setClipboard)
