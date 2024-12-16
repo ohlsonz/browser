487d36a92df3a51835484e96e3135a776b3d441b
Leo Ohlson <leo@ohlson.dev>
2024-12-16 17:38:01 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
---- @param value string
+---@param value string
 local function setClipboard(value)
 	assert(type(value) == "string", ("Failed to copy to clipboard, expected string. Got %s"):format(type(value)))
 
