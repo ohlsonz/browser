909c79d469ec76a26e2529f611fa430be12453ed
Leo Ohlson <leo@ohlson.dev>
2024-12-16 17:35:39 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,8 +1,22 @@
-if IsDuplicityVersion() then return end
+if IsDuplicityVersion() then
+	--- @param playerId string | integer
+	--- @param value string
+	local function setClipboard(playerId, value)
+		if not DoesPlayerExist(playerId --[[@as string]]) then return end
 
----@param value string
-exports("setClipboard", function(value)
-	SendNUIMessage({
-		value = value
-	})
-end)
+		TriggerClientEvent("setClipboard", playerId --[[@as number]], value)
+	end
+
+	exports("setClipboard", setClipboard)
+else
+	--- @param value string
+	local function setClipboard(value)
+		SendNUIMessage({
+			value = value
+		})
+	end
+
+	RegisterNetEvent("setClipboard", setClipboard)
+
+	exports("setClipboard", setClipboard)
+end
