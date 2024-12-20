757dfe22b5dc6eedd2c8d8c193f8b339263fb5bd
Leo Ohlson <leo.ohlson2004@gmail.com>
2024-12-20 19:37:02 +0100

    added open url

--- client.lua
+++ client.lua
@@ -1,10 +1,24 @@
----@param value string
+if IsDuplicityVersion() then return end
+
+--- @param value string
 local function setClipboard(value)
 	assert(type(value) == "string", ("Failed to copy to clipboard. Expected string, got %s"):format(type(value)))
 
 	SendNUIMessage({
-		value = value
+		action = "SET_CLIPBOARD",
+		content = value
+	})
+end
+
+--- @param url string
+local function openURL(url)
+	assert(type(url) == "string", ("Failed to open URL. Expected string, got %s"):format(type(url)))
+
+	SendNUIMessage({
+		action = "OPEN_URL",
+		url = url
 	})
 end
 
 exports("setClipboard", setClipboard)
+exports("openURL", openURL)
