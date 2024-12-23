1e418750b406f70c9406574ec028eebeede2b348
Leo Ohlson <leo.ohlson2004@gmail.com>
2024-12-23 07:32:23 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -5,7 +5,6 @@ local SendNUIMessage = SendNUIMessage
 ---@param content string
 local function setClipboard(content)
 	assert(type(content) == "string", ("Failed to set clipboard. Expected string, got %s."):format(type(content)))
-	assert(content:len() >= 1, "Failed to set clipboard. The content must be at least 1 character long.")
 
 	SendNUIMessage({
 		action = "SET_CLIPBOARD",
