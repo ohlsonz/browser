aef5a7defa35f08e02df5fca8dc4ec7f4b774c3b
Leo Ohlson <leo.ohlson2004@gmail.com>
2024-12-23 07:30:25 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -4,7 +4,8 @@ local SendNUIMessage = SendNUIMessage
 
 ---@param content string
 local function setClipboard(content)
-	assert(type(content) == "string", ("Failed to set clipboard. Expected string, got %s"):format(type(content)))
+	assert(type(content) == "string", ("Failed to set clipboard. Expected string, got %s."):format(type(content)))
+	assert(content:len() >= 1, ("Failed to set clipboard. The content (%s) must be at least 1 character long."):format(content))
 
 	SendNUIMessage({
 		action = "SET_CLIPBOARD",
@@ -14,7 +15,8 @@ end
 
 ---@param url string
 local function openURL(url)
-	assert(type(url) == "string", ("Failed to open URL. Expected string, got %s"):format(type(url)))
+	assert(type(url) == "string", ("Failed to open URL. Expected string, got %s."):format(type(url)))
+	assert(url:len() >= 1, ("Failed to open URL. The URL (%s) must be at least 1 character long."):format(url))
 
 	SendNUIMessage({
 		action = "OPEN_URL",
